a8cd28be11ca421068ee3fe5086302f9aa1d1054
Sisasenkosi Sibanda <sisasenkosisibanda98@gmail.com>
2025-05-29 20:25:02 +0200

    first commit

---
+++ aos.js
@@ -0,0 +1,8 @@
+// We initialise some arrtibutes to customise our view as we initialise our AOS
+// AOS.init();
+AOS.init({
+  delay: 150, // values from 0 to 3000, with step 50ms
+  duration: 1550, // values from 0 to 3000, with step 50ms
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+});
---
+++ navbar.js
@@ -0,0 +1,7 @@
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  document.getElementById('mobile-menu').classList.remove('hidden');
+});
+
+document.getElementById('close-menu').addEventListener('click', function () {
+  document.getElementById('mobile-menu').classList.add('hidden');
+});
